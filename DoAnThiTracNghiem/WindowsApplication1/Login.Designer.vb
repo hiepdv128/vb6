171b4fca1bc4251133171e8ea21ffe38ad21be58
hiepdv128 <Hiepit3>
2016-05-12 11:27:57 +0700

    a
    a

---
+++ DoAnThiTracNghiem/WindowsApplication1/Login.Designer.vb
@@ -0,0 +1,117 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class login
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.txtUser = New System.Windows.Forms.TextBox()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.txtPass = New System.Windows.Forms.TextBox()
+        Me.cmdLogin = New System.Windows.Forms.Button()
+        Me.lblAbout = New System.Windows.Forms.Label()
+        Me.SuspendLayout()
+        '
+        'txtUser
+        '
+        Me.txtUser.Font = New System.Drawing.Font("Microsoft Sans Serif", 14.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.txtUser.Location = New System.Drawing.Point(359, 115)
+        Me.txtUser.Name = "txtUser"
+        Me.txtUser.Size = New System.Drawing.Size(170, 29)
+        Me.txtUser.TabIndex = 0
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Font = New System.Drawing.Font("Microsoft Sans Serif", 14.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label1.Location = New System.Drawing.Point(235, 118)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(93, 24)
+        Me.Label1.TabIndex = 1
+        Me.Label1.Text = "Tài khoản"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Font = New System.Drawing.Font("Microsoft Sans Serif", 14.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label2.Location = New System.Drawing.Point(235, 189)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(86, 24)
+        Me.Label2.TabIndex = 3
+        Me.Label2.Text = "Mật khẩu"
+        '
+        'txtPass
+        '
+        Me.txtPass.Font = New System.Drawing.Font("Microsoft Sans Serif", 14.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.txtPass.Location = New System.Drawing.Point(359, 186)
+        Me.txtPass.Name = "txtPass"
+        Me.txtPass.Size = New System.Drawing.Size(170, 29)
+        Me.txtPass.TabIndex = 2
+        Me.txtPass.UseSystemPasswordChar = True
+        '
+        'cmdLogin
+        '
+        Me.cmdLogin.Font = New System.Drawing.Font("Microsoft Sans Serif", 14.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.cmdLogin.Location = New System.Drawing.Point(327, 263)
+        Me.cmdLogin.Name = "cmdLogin"
+        Me.cmdLogin.Size = New System.Drawing.Size(138, 36)
+        Me.cmdLogin.TabIndex = 4
+        Me.cmdLogin.Text = "Login"
+        Me.cmdLogin.UseVisualStyleBackColor = True
+        '
+        'lblAbout
+        '
+        Me.lblAbout.AutoSize = True
+        Me.lblAbout.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblAbout.Location = New System.Drawing.Point(766, 438)
+        Me.lblAbout.Name = "lblAbout"
+        Me.lblAbout.Size = New System.Drawing.Size(74, 16)
+        Me.lblAbout.TabIndex = 5
+        Me.lblAbout.Text = "@About us"
+        '
+        'login
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(852, 469)
+        Me.Controls.Add(Me.lblAbout)
+        Me.Controls.Add(Me.cmdLogin)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.txtPass)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.txtUser)
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog
+        Me.IsMdiContainer = True
+        Me.MaximizeBox = False
+        Me.Name = "login"
+        Me.Text = "Login"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents txtUser As System.Windows.Forms.TextBox
+    Friend WithEvents Label1 As System.Windows.Forms.Label
+    Friend WithEvents Label2 As System.Windows.Forms.Label
+    Friend WithEvents txtPass As System.Windows.Forms.TextBox
+    Friend WithEvents cmdLogin As System.Windows.Forms.Button
+    Friend WithEvents lblAbout As System.Windows.Forms.Label
+
+End Class
---
+++ DoAnThiTracNghiem/WindowsApplication1/Login.vb
@@ -0,0 +1,19 @@
+﻿Imports System.Data.SqlClient
+Public Class login
+    Private Sub cmdLogin_Click(sender As Object, e As EventArgs) Handles cmdLogin.Click
+        Dim connectstring As String = "Data Source=DESKTOP-6N5I9TS;Initial Catalog=DB_Demo;Integrated Security=True"
+        Dim con As New SqlConnection(connectstring)
+        con.Open()
+        Dim cmd As New SqlCommand("select password from alluser where username = @username", con)
+        cmd.Parameters.AddWithValue("@username", txtUser.Text)
+        Dim result As String = cmd.ExecuteScalar().ToString
+
+        If (result.Equals(txtPass.Text)) 
+            MsgBox("Dang nhap thanh cong")
+        End If
+    End Sub
+
+    Private Sub lblAbout_Click(sender As Object, e As EventArgs) Handles lblAbout.Click
+        about.Show()
+    End Sub
+End Class
